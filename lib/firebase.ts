dcac4bd575b4e33631f90a87b83045918fc8a2da
muathith <muathith@outlook.com>
2026-03-15 09:26:38 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,7 +5,14 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // Paste config here
+ apiKey: "AIzaSyBZ92rFcpDHjOa74Ki9I7Rhhmz-dgEHWcs",
+  authDomain: "school-77936.firebaseapp.com",
+  databaseURL: "https://school-77936-default-rtdb.firebaseio.com",
+  projectId: "school-77936",
+  storageBucket: "school-77936.firebasestorage.app",
+  messagingSenderId: "783345000886",
+  appId: "1:783345000886:web:292770d1da6371330ca117",
+  measurementId: "G-2QSLKE8W6S"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
